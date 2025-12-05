006505f0ecf38629bbc48692739b92cf8f4b13aa
kylewisialowski <103215823+kylewisialowski@users.noreply.github.com>
2025-12-05 14:48:16 -0500

    trying to configure data prep stuff

--- toad_tf/models_tf/resnet_tf.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-def resnet50_baseline(pretrained=True):
-    return tf.keras.applications.ResNet50(
-        include_top=False,        
-        weights='imagenet' if pretrained else None,
-        pooling='avg'             
-    )
