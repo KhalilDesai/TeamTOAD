168b1b2aec80fb3cd552de374c123150830915f8
kylewisialowski <103215823+kylewisialowski@users.noreply.github.com>
2025-12-08 15:50:19 -0500

    adding feature extraction scripts

---
+++ our_data_prep/model.py
@@ -0,0 +1,18 @@
+import os
+from functools import partial
+import timm
+from timm_wrapper import TimmCNNEncoder
+import torch
+from constants import MODEL2CONSTANTS
+from transform_utils import get_eval_transforms
+        
+def get_encoder(model_name, target_img_size=224):
+    print('loading model checkpoint')
+    model = TimmCNNEncoder()
+    print(model)
+    constants = MODEL2CONSTANTS[model_name]
+    img_transforms = get_eval_transforms(mean=constants['mean'],
+                                         std=constants['std'],
+                                         target_img_size = target_img_size)
+
+    return model, img_transforms
