6b53aa0426aabd0ffb7ed75495dfcc71bc0ebbb7
KhalilDesai <khalil_desai@brown.edu>
2025-12-08 00:23:35 -0500

    wtf

--- run_patching.sh
+++ run_patching.sh
@@ -5,9 +5,15 @@
 #SBATCH -t 03:00:00             # total run time limit (HH:MM:SS)
 #SBATCH --mem=120GB        # INCREASED from 16GB to 32GB
 #SBATCH --job-name='patching'
+
 #SBATCH --output=slurm_logs/R-%x.%j.out
 #SBATCH --error=slurm_logs/R-%x.%j.err
+
 #SBATCH --mail-type=END         # Sends email when the job finishes
 #SBATCH --mail-user=your_email@example.com   # Replace with your email
+
+
+module load miniconda3/23.11.0s
+conda activate final_project
 
 python create_patches_fp.py --source TOAD_toy_data --save_dir patches_dir --patch_size 256 --seg --patch --stitch 
