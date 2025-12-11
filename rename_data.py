ce7380bfc2a806fd2318e16b1dc772ca584638ae
KhalilDesai <khalil_desai@brown.edu>
2025-12-10 21:43:38 -0500

    change rename

--- rename_data.py
+++ rename_data.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 # Define the root folder containing the cancer type folders
-root_folder = 'TOAD_dataset_full'
+root_folder = 'TOAD_dataset_testing'
 
 # List of cancer types (folders)
 cancer_types = ['BRCA', 'COAD', 'CCRCC', 'LUAD']
